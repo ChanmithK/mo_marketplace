625e2c473d2f04d2f2591e66a91a962ec72f01f2
Praveen Kariyawasam <86306349+ChanmithK@users.noreply.github.com>
2026-04-03 21:25:48 +0530

    feat: Created Required Components

---
+++ mo-marketplace-web/src/components/ui/LoadingSpinner.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const LoadingSpinner: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
+    </div>
+  );
+};
+
+export const SmallLoadingSpinner: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-blue-600"></div>
+    </div>
+  );
+};
